7f95affdc49ba958660093fe0f49654f6a2fa6eb
MaxVoltage52 <80371312+MaxVoltage52@users.noreply.github.com>
2025-09-20 17:21:57 -0500

    Update switch.jsx

--- src/components/ui/switch.jsx
+++ src/components/ui/switch.jsx
@@ -1,11 +1,12 @@
-export function Switch({ checked, onCheckedChange, id, ...props }) {
+export function Switch({ checked, onCheckedChange, id, className = "", ...props }) {
   return (
     <input
       type="checkbox"
       id={id}
       checked={!!checked}
       onChange={(e) => onCheckedChange?.(e.target.checked)}
+      className={`h-5 w-5 rounded border-gray-300 text-blue-600 focus:ring-blue-500 ${className}`}
       {...props}
     />
-  )
+  );
 }
