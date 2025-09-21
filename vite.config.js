6bcfa5fc2d996da120f14164773a8536f213b0e4
MaxVoltage52 <80371312+MaxVoltage52@users.noreply.github.com>
2025-09-21 10:15:17 -0500

    Update vite.config.js
    Changing to github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+  // IMPORTANT: replace REPO_NAME with your repository name
+  base: '/heatpump-calculator/',
 })
